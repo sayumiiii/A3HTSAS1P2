054ba202bf5210868d19d81c679a50681910f3ab
GeethOnion <geethmaka@gmail.com>
2021-03-15 14:25:02 +0530

    fix:bug fixes

--- Program.cs
+++ Program.cs
@@ -22,48 +22,26 @@ namespace A3HTSAS1P2
     {
         static void Main(string[] args)
         {
-            int num1 = 0;
-            int num2 = 0;
-            int num3 = 0;
+            string num1,num2,num3
+            int n1,n2,n3=0;
             string option = "";
             bool i = true;
 
-            if (i == true)
+            while (i)
             {
+                Console.WriteLine("Enter number 1: ");
+                num1 = Console.ReadLine();
+                Console.WriteLine("Enter number 2: ");
+                num2 = Console.ReadLine();
+                Console.WriteLine("Enter number 3: ");
+                num3 = Console.ReadLine();
+
                 try
                 {
-                    Console.WriteLine("Enter number 1: ");
-                    num1 = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter number 2: ");
-                    num2 = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Enter number 3: ");
-                    num3 = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("a. Calculate working percentage ");
-                    Console.WriteLine("b. multiply");
-                    Console.WriteLine("c. add ");
-                    Console.WriteLine("Choose an option: ");
-                    option=Console.ReadLine();
-
-                   if (option == "a")
-                    {                           
-                        Console.WriteLine("First number/ second number multiplied by 100: " + (num1 / num2) * 100 + "%");                            
-                    }
-                   else if (option== "b")
-                    {
-                        Console.WriteLine("result of multiplying all numbers:" + num1*num2*num3)
-                    }
-                   else if (option== "c")
-                    {
-                        Console.WriteLine("Result of addition os all numbers" + num1+num2+num3);                     
-                    }
-                   else
-                    {
-                        Console.WriteLine("please enter only a, b, or c ");
-                        i = true;
-                    }
-
-                    i = false;
+                    n1=int.Parse(num1);
+                    n2=int.Parse(num2);
+                    n3=int.Parse(num3);
+                    i=false;
                 }
 
                 catch (FormatException)
@@ -83,7 +61,28 @@ namespace A3HTSAS1P2
                     i = true;
                 }
             }
-            
+
+            i=true;
+            while(i){
+                Console.WriteLine("a. Calculate working percentage ");
+                Console.WriteLine("b. multiply");
+                Console.WriteLine("c. add ");
+                Console.WriteLine("Choose an option: ");
+                option=Console.ReadLine();
+
+                if (option == "a"){                           
+                    Console.WriteLine("First number/ second number multiplied by 100: " + (num1 / num2) * 100 + "%");                            
+                }
+                else if (option== "b"){
+                    Console.WriteLine("result of multiplying all numbers:" + num1*num2*num3)
+                }
+                else if (option== "c"){
+                    Console.WriteLine("Result of addition os all numbers" + num1+num2+num3);                     
+                }
+                else{
+                    Console.WriteLine("please enter only a, b, or c ");
+                }
+            }
         }
     }
 }
